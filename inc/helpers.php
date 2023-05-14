8d08ba96271fa46020127e66284f415b82c6bf2a
Dave Pullig <dave@deliciousmedia.co.uk>
2023-05-14 16:05:17 +0100

    Tidy docblock formatting

--- inc/helpers.php
+++ inc/helpers.php
@@ -122,13 +122,13 @@ if ( ! function_exists( 'dm_remove_filters_for_anonymous_class' ) ) {
 /**
  * Replacement for get_page_by_title() which is deprecated as of 6.2.
  *
- * @param string       $page_title      Page title.
+ * @param string       $page_title Page title.
  * @param string       $output     Optional. The required return type. One of OBJECT, ARRAY_A, or ARRAY_N, which
  *                                 correspond to a WP_Post object, an associative array, or a numeric array,
  *                                 respectively. Default OBJECT.
  * @param string|array $post_type  Optional. Post type or array of post types. Default 'page'.
- * @return WP_Post|array|null WP_Post (or array) on success, or null on failure.
  *
+ * @return WP_Post|array|null      WP_Post (or array) on success, or null on failure.
  * @link   https://make.wordpress.org/core/2023/03/06/get_page_by_title-deprecated/
  */
 function dm_get_page_by_title( $page_title, $output = OBJECT, $post_type = 'page' ) {
